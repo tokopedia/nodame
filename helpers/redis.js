235703a61a3f4a8d46987a76b06c81699cf77533
hKyo89 <hkyo89@yahoo.co.jp>
2015-06-17 12:08:10 +0700

    Update redis.js

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,44 +1,34 @@
-var redis = require("redis");
-var md5 = require('MD5');
-var jumphash = require('jumphash');
-
-var parent = this;
-
-var redisPool = {};
+var redis       = require("redis");
+var md5         = require('MD5');
+var jumphash    = require('jumphash');
+var parent      = this;
+var redisPool   = {};
 
 exports.getClient = function (mapShard, replication, key) {
     var configRedis = helper.config.get('redis');
     var conn        = configRedis.map[mapShard];
 
-    if(conn.length > 0) {
-        if(!replication) {
+    if (conn.length > 0) {
+        if (!replication) {
             replication = 'master';
         }
-        if(!key) {
+        
+        if (!key) {
             key = 1;
         }
 
-        // console.log('mapShard', mapShard);
-        // console.log('replication', replication);
-        // console.log('key', key);
-
-        var indexShard = jumphash(key, conn.length);
-        // console.log('indexShard', indexShard);
-
-        var connectionString = 'redis:' +  conn[indexShard] + ':' + replication;
-        // console.log('connectionString', connectionString);
-
-        var connectionId = md5(connectionString);
-        // console.log('connectionId', connectionId);
-        // console.log('conn', conn);
-
-        var redisGroup = configRedis[conn[indexShard]];
-
-        var redisConTemp = redisGroup[replication].split(':');
-        // console.log('redisConTemp', redisConTemp);
-
-        if(!redisPool[connectionId]) {
+        var indexShard          = jumphash(key, conn.length);
+        var connectionString    = 'redis:' +  conn[indexShard] + ':' + replication;
+        var connectionId        = md5(connectionString);
+        var redisGroup          = configRedis[conn[indexShard]];
+        var redisConTemp        = redisGroup[replication].split(':');
+        
+        if (!redisPool[connectionId]) {
             redisPool[connectionId] = redis.createClient(redisConTemp[1], redisConTemp[0], {});
+
+            redisPool[connectionId].on("error", function (err) {
+                console.log("Error Redis " + err);
+            });
         }
 
         return redisPool[connectionId];
