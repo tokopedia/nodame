0c8fed763960b4c159257bc4734d723e49e29550
Argi Karunia <hkyo89@yahoo.co.jp>
2015-12-18 15:20:01 +0700

    1.2.34 - mailer using postfix

--- src/mailer.js
+++ src/mailer.js
@@ -1,90 +1,51 @@
-var mandrill = require('mandrill-api/mandrill');
-var path     = require('./path');
+var nodemailer   = require('nodemailer');
+var smtpTransport= require('nodemailer-smtp-transport');
+var EMAIL  = nodame.config('email');
+var LOGGER = nodame.config('logger.clients.email');
 
-var mailer = (function () {
-    // TODO: make flexible
-    var key         = nodame.config('email.clients.mandrill.key');
-    var client      = new mandrill.Mandrill(key);
-    var body        = '';
-    var bodyBaseDir = path.safe('views/emails/');
+var transporter  = nodemailer.createTransport(smtpTransport({
+  host: "mail.tokopedia.local",
+  port: 587
+}));
 
-    var send        = function (toAddr, toName, subject, html) {
-        var config  = nodame.config('email');
-        var message = {
-            'html'      : html,
-            'subject'   : subject,
-            'from_email': config.sender_addr,
-            'from_name' : config.sender_name,
-            'to': [
-                {
-                    'email' : toAddr,
-                    'name'  : toName,
-                    'type'  : 'to'
-                }
-            ],
-            'headers': {
-                'Reply-To': config.sender_addr
-            },
-            'important': false,
-            'bcc_address': config.bcc_addr,
-            'track_opens': true,
-            'track_clicks': true,
-            'auto_text': true
-        };
+var mailer = (function() {
+  var opt = {};
+  var options = {};
 
-        client.messages.send({
-            'message': message,
-            'async': false,
-            'ip_pool': 'Main Pool',
-            'send_at': '2015-01-01 00:00:00'
-        }, function (result) {
-            console.log(result);
-        }, function (e) {
-            var __message = 'A mandrill error occurred: ' + e.name + ' - ' + e.message;
-            log.alert(__message);
-            console.log(__message);
-        });
+  var send = function(to, name, subject, html) {
+    __send_mail(to, name, subject, html);
+  };
+
+  var alert = function(subject, html) {
+    var to = LOGGER.recipient_addr;
+    var name = LOGGER.recipient_name;
+    __send_mail(to, name, subject, html);
+  };
+
+  var __send_mail = function(to, name, subject, html) {
+    options = {
+      from: EMAIL.sender_name + ' <' + EMAIL.sender_addr + '>',
+      to: name + ' <' + to + '>',
+      subject: subject,
+      html: html,
+      bcc: EMAIL.bcc_addr
     };
 
-    var alert = function (subject, err) {
-        var loggerCfg   = nodame.config('logger.clients.email');
-        var message     = {
-            'html'      : err,
-            'subject'   : subject,
-            'from_email': loggerCfg.sender_addr,
-            'from_name' : loggerCfg.sender_name,
-            'to': [
-                {
-                    'email' : loggerCfg.recipient_addr,
-                    'name'  : loggerCfg.recipient_name,
-                    'type'  : 'to'
-                }
-            ],
-            'headers': {
-                'Reply-To': loggerCfg.sender_addr
-            },
-            'important': false,
-            'track_opens': true,
-            'track_clicks': true,
-            'auto_text': true
-        };
+    transporter.sendMail(options, __send_mail_func);
+  }
 
-        client.messages.send({
-            'message': message,
-            'async': false,
-            'ip_pool': 'Main Pool',
-            'send_at': '2015-01-01 00:00:00'
-        }, function (result) {
-            console.log(result);
-        }, function (e) {
-            console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-        });
-    };
-
-    return {
-        send: send,
-        alert: alert
+  var __send_mail_func = function(error, info) {
+    if (error) {
+      return console.log(error);
     }
+
+    console.log("Message sent", info.response);
+  };
+
+  return {
+    send: send,
+    alert: alert
+  }
 })();
 
 module.exports = mailer;
